010a57f656fa8d6bd376d3d4a40d196acf7fc287
javierhuebra <javieremanuelhuebra@gmail.com>
2023-06-21 13:58:04 -0300

    modificando algoapp archivo

--- Huebra_AlgoApp/Huebra_AlgoApp.js
+++ Huebra_AlgoApp/Huebra_AlgoApp.js
@@ -1,2 +1,4 @@
 // Los ejercicios de la algoapp están resueltos en su totalidad y también el examen calificado con 9/10 bajo el mail de javieremanuelhuebra@gmail.com
 //En este momento la plataforma no me deja entrar para poder hacer una captura
+
+//Asimismo todos los tutoriales para preparar el entorno de desarrollo junto con los apuntes teóricos fueron estudiados en su totalidad
