28d81a05287650ad58acf44c4ebdb779a1f1bf7e
javierhuebra <javieremanuelhuebra@gmail.com>
2023-06-21 03:57:05 -0300

    ejemplo de la montaña rusa

---
+++ Huebra_Javier_JS_Jugueteo/Huebra_Javier_JS_Jugueteo.js
@@ -0,0 +1,9 @@
+let alturaNinio = 20
+
+const muestraSiElNinioPuedeSubirALaMontaniaRusa = (alturaNinio) =>{
+    if(alturaNinio > 52) console.log("¡Súbete Chico!")
+    
+    else console.log("Lo siento, chico. Tal vez el próximo año")
+}
+
+muestraSiElNinioPuedeSubirALaMontaniaRusa(alturaNinio)
