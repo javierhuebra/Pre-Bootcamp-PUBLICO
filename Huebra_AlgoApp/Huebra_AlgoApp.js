ce5776336a393dbae73fc7769aee62d96fa5fd47
javierhuebra <javieremanuelhuebra@gmail.com>
2023-06-21 13:48:51 -0300

    Ejercicios algoapp y mas

---
+++ Huebra_AlgoApp/Huebra_AlgoApp.js
@@ -0,0 +1,2 @@
+// Los ejercicios de la algoapp están resueltos en su totalidad y también el examen calificado con 9/10 bajo el mail de javieremanuelhuebra@gmail.com
+//En este momento la plataforma no me deja entrar para poder hacer una captura
