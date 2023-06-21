3d2869a1454de33b8671412af170777b77d5fedb
javierhuebra <javieremanuelhuebra@gmail.com>
2023-06-21 02:52:31 -0300

    Prediccion ejercicio

---
+++ Huebra_Javier_JSPrediccion/Huebra_Javier_JSPrediccion.js
@@ -0,0 +1,33 @@
+//Predicción 1-----------------------------------
+
+function myBirthYearFunc() {
+    console.log("Nací en " + 1980); //Imprime un string que dice "Nací en 1980"
+}
+
+
+//Predicción 2-----------------------------------
+
+function myBirthYearFunc(EntradaAñoNacimiento) {
+    console.log("Nací en " + EntradaAñoNacimiento); //Al igual que la anterior imprime un string que dice "Nací en 1980"
+}
+
+//Predicción 3-----------------------------------
+
+function add(num1, num2) {
+    console.log("¡Sumando números!"); //Imprime ¡Sumando números!
+    console.log("num1 is: " + num1);  //Imprime num1 is: 10
+    console.log("num2 is: " + num2);  //Imprime num2 is: 20
+    var sum = num1 + num2;
+    console.log(sum);                 //Imprime 30
+}
+
+
+
+
+
+
+
+
+
+
+
