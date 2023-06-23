a6988ed80cb6bd9dafde4e8b532a4361640b1a46
javierhuebra <javieremanuelhuebra@gmail.com>
2023-06-23 16:22:41 -0300

    arreglos finales

--- Huebra_T_D_Return/Huebra_T_D_Return.js
+++ Huebra_T_D_Return/Huebra_T_D_Return.js
@@ -119,3 +119,67 @@ console.log('result is', result);
 // El resultado se asigna a la variable result.
 
 // Paso 3: Impresión del texto 'result is' seguido del valor de la variable result
+
+
+//FRAGMENTO SIETE
+function sumNums(num1, num2) {  
+    return num1+num2;
+ }
+ console.log(sumNums(2,3))
+ console.log(sumNums(3,5))
+ // Paso 1 : No hay variables declaradas, solo las que ingresan a la funcion
+ // La funcion declarada retorna la suma de las variables que entran en el parametro (num1 y num2)
+ // Se llama dentro de un console log una primera vez a la funcion con valores 2 y 3, retorna 5 e imprime 5
+ // Se llama dentro de un console log una segunda vez a la funcion con valores 3 y 5, retorna 8 e imprime 8
+
+
+ //FRAGMENTO OCHO
+ function printSumNums(num1, num2) {
+    console.log(num1);   
+    return num1+num2;
+ }
+ console.log(printSumNums(2,3))
+ console.log(printSumNums(3,5))
+//Paso 1: No hay variables mas alla de las que entran como parametro en la funcion
+// Se imprime el valor de num1 dentro de la funcion
+//Retorna la suma de los valores num1 y num2
+//Se llama la funcion dentro de un console log con valores 2 y 3, retorna 5 e imprime 5 
+//Se llama la funcion dentro de un console log con valores 3 y 5, retorna 8 e imprime 8
+
+
+
+//FRAGMENTO NUEVE
+function sumNums(num1, num2) {
+    var sum = num1 + num2;
+    console.log('sum is', sum);
+    return sum;
+}
+var result = sumNums(2,3) + sumNums(3,5);
+console.log('result is', result);
+
+// La funcion recibe parametros num1 y num2
+// Suma los valores de num1 y num2 y los asigna a la variable sum
+// Imprime en la consola el mensaje 'sum is' seguido del valor de sum
+// Retorna el valor de sum
+// Llama a la función sumNums dos veces y suma los resultados, asignando el valor a la variable result
+// Imprime en la consola el mensaje 'result is' seguido del valor de result
+
+
+
+
+//FRAGMENTO DIEZ
+function sumNums(num1, num2) {
+    var sum = num1 + num2;
+    console.log('sum is', sum);
+    return sum;
+}
+var result = sumNums(2,3) + sumNums(3,sumNums(2,1)) + sumNums(sumNums(2,1),sumNums(2,3));
+console.log('result is', result);
+
+// Suma los valores de num1 y num2 y los asigna a la variable sum
+// Imprime en la consola el mensaje 'sum is' seguido del valor de sum
+// Retorna el valor de sum
+// Llama a sumNums con los argumentos (2, 3)
+// Llama a sumNums con los argumentos (2, 1) y luego llama a sumNums con los argumentos (3, resultado anterior)
+// Llama a sumNums con los argumentos (2, 1) y luego llama a sumNums con los argumentos (2, 3)
+// Imprime en la consola el mensaje 'result is' seguido del valor de result
