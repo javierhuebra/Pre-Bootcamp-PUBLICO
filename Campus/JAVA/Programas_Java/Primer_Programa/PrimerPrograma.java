a4ac77d8efc34a8df276d2235113edaffa76d845
Javier Emanuel Huebra <104619744+javierhuebra@users.noreply.github.com>
2023-07-12 12:05:58 -0300

    agregando programa java

---
+++ Campus/JAVA/Programas_Java/Primer_Programa/PrimerPrograma.java
@@ -0,0 +1,11 @@
+public class PrimerPrograma{
+    public static void main(String[] args){
+        
+        String nombre = "Javier Huebra";
+        int edad = 100; 
+        String ciudadNatal = "Bahía Blanca";
+
+        System.out.println("Mi nombre es "+ nombre+" tengo "+edad+" años y vivo en "+ ciudadNatal);
+        
+    }
+}
