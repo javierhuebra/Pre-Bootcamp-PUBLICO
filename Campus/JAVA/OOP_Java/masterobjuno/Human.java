4edfb53e2620f17b45bae7a5af5347f480291167
Javier Emanuel Huebra <104619744+javierhuebra@users.noreply.github.com>
2023-07-17 02:18:44 -0300

    cargando actividad

--- Campus/JAVA/OOP_Java/masterobjuno/Human.java
+++ Campus/JAVA/OOP_Java/masterobjuno/Human.java
@@ -3,11 +3,20 @@ package OOP_Java.masterobjuno;
 public class Human {
     private int strength, stealth, intelligence, health;
 
+    //Metodo para instanciar por default
     public Human(){
         strength = 3;
         stealth = 3;
         intelligence = 3;
         health = 100;
+    }
+
+    //Metodo para instanciar con valores
+    public Human(int strength, int stealth, int intelligence,int health){
+        this.strength = strength;
+        this.stealth = stealth;
+        this.intelligence = intelligence;
+        this.health = health;
     }
 
     public void attack(Human atacado){
